b5b7d6d4790e1fbf33707bc0557bb0c520b69b48
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-08 17:18:14 +0200

    métodos reescritos para TaskModel

--- app/models/TaskModel.class.php
+++ app/models/TaskModel.class.php
@@ -30,9 +30,45 @@ class Task extends Model
     return $this->tasks;
   }
 
+  public function fetchOne($id): ?object
+  {
+    foreach ($this->tasks as $task) {
+      if ($task->id === $id) {
+        return $task;
+      }
+    }
+  }
 
-  //TODO reescribir estos métodos
-  // fetchOne
-  //save
-  //delete
+  public function save($data = array()): bool
+  {
+    // Asignar un nuevo ID basado en el tamaño del array actual
+    $newId = count($this->tasks) + 1;
+
+    // Crear una nueva tarea como objeto genérico (stdClass)
+    $newTask = (object) [
+      'id' => $newId,
+      'name' => $data['name'],
+      'description' => $data['description']
+    ];
+
+    // Agregar la nueva tarea a la lista de tareas
+    $this->tasks[] = $newTask;
+
+    // Intentar guardar el array actualizado de tareas en el archivo JSON
+    $result = file_put_contents($this->taskFile, json_encode($this->tasks));
+
+    // Devolver true si el resultado es válido, de lo contrario false
+    return $result !== false; // file_put_contents devuelve false en caso de error
+  }
+
+  public function delete($id): bool
+  {
+    foreach ($this->tasks as $key => $task) {
+      if ($task->id === $id) {
+        unset($this->tasks[$key]);
+        return true;
+      }
+      return false;
+    }
+  }
 }
